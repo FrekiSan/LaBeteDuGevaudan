1e8fd54e8a6beea472555c5804a2499245199b95
Freki <maxime.orange63@gmail.com>
2025-10-22 18:26:29 +0200

    escape

---
+++ importmap.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'imports' => [
+        // Entry-point de ton front :
+        'app' => [
+            'path' => './assets/js/app.js',
+            'entrypoint' => true,
+        ],
+    ],
+];
